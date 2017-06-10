1472d4ecb49bf4ffc316181934a4daea0a212742
Susan Lau <susan.lau@genj.nl>
2017-06-10 17:05:03 +0200

    add isActive, publishAt + expiresAt to editable field list

--- src/Genj/FaqAdminBundle/Admin/QuestionAdmin.php
+++ src/Genj/FaqAdminBundle/Admin/QuestionAdmin.php
@@ -2,6 +2,7 @@
 
 namespace Genj\FaqAdminBundle\Admin;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -15,8 +16,8 @@ use Sonata\AdminBundle\Form\FormMapper;
 class QuestionAdmin extends Admin
 {
     protected $datagridValues = array(
-        '_page' => 1,
-        '_sort_by' => 'issueDate',
+        '_page'       => 1,
+        '_sort_by'    => 'issueDate',
         '_sort_order' => 'Desc'
     );
 
@@ -29,6 +30,7 @@ class QuestionAdmin extends Admin
             ->add('headline')
             ->add('body')
             ->add('category')
+            ->add('isActive')
             ->add('slug');
     }
 
@@ -42,11 +44,13 @@ class QuestionAdmin extends Admin
             ->add('headline', null, array('identifier' => true))
             ->add('Category')
             ->add('rank')
+            ->add('isActive')
+            ->add('publishAt')
             ->add('_action', 'actions',
                 array(
                     'actions' => array(
-                        'show' => array(),
-                        'edit' => array(),
+                        'show'   => array(),
+                        'edit'   => array(),
                         'delete' => array()
                     )
                 )
@@ -59,15 +63,32 @@ class QuestionAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('headline', null, array('attr' => array('class' => 'span12')))
-            ->add('body', null, array('required' => false, 'attr' => array('class' => 'span12')))
-            ->add('category', null, array(
+            ->with('Basics', array('class' => 'col-md-8'))
+                ->add('headline')
+                ->add('body', null, array('required' => false))
+            ->end()
+
+            ->with('Status', array('class' => 'col-md-4'))
+                ->add('isActive', ChoiceType::class, array(
+                        'choices'  => array(
+                            'draft'     => 0,
+                            'published' => 1
+                        ),
+                        'expanded' => true,
+                        'required' => true,
+                        'label'    => 'Status',
+                        'attr'     => array('class' => 'radio-list')
+                    )
+                )
+                ->add('publishAt', null, array('date_format' => 'd M y'))
+                ->add('expiresAt', null, array('date_format' => 'd M y'))
+                ->add('rank', null, array('required' => false))
+                ->add('category', null, array(
                     'expanded' => true,
-                    'required' => true,
-                    'attr' => array('class' => 'radio-list vertical')
-                ))
-            ->add('rank', null, array('required' => false, 'attr' => array('class' => 'span12')))
-            ->add('slug', null, array('required' => false, 'attr' => array('class' => 'span12')))
+                    'required' => true
+                    )
+                )
+                ->add('slug', null, array('required' => false))
             ->end();
     }
 }
